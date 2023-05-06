c9d5283f43e2514adb9fa389df15dd7217d4b7be
Daegyun Jung <jdk829355@gmail.com>
2023-05-07 06:35:16 +0900

    completed serverStreaming server and client

--- server_streaming/client/client.go
+++ server_streaming/client/client.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"context"
+	"flag"
+	pb "github.com/jdk829355/go_fssn/server_streaming/serverStreaming"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"io"
+	"log"
+)
+
+var serverAddr = flag.String("server_addr", "localhost:50051", "The server address with port")
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to dial: %v", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+	}(conn)
+
+	client := pb.NewServerStreamingClient(conn)
+	stream, err := client.GetServerResponse(context.Background(), &pb.Number{Value: 1})
+	if err != nil {
+		log.Fatalf("GetServerResponse - %v", err)
+	}
+
+	for {
+		content, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("GetServerResponse stream - %v", err)
+		}
+
+		log.Printf("Message: message: %s", content.GetMessage())
+	}
+}
--- server_streaming/server/server.go
+++ server_streaming/server/server.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"flag"
+	pb "github.com/jdk829355/go_fssn/server_streaming/serverStreaming"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"strings"
+)
+
+var (
+	addr = flag.String("port", ":50051", "The server port")
+)
+
+type server struct {
+	pb.UnimplementedServerStreamingServer
+}
+
+func makeMessage(s string) pb.Message {
+	return pb.Message{Message: s}
+}
+
+func (s *server) GetServerResponse(req *pb.Number, stream pb.ServerStreaming_GetServerResponseServer) error {
+	messages := []pb.Message{
+		makeMessage("message #1"),
+		makeMessage("message #2"),
+		makeMessage("message #3"),
+		makeMessage("message #4"),
+		makeMessage("message #5"),
+	}
+	log.Printf("Server processing gRPC server-streaming {%d}.", req.Value)
+	for i := 0; i < 5; i++ {
+		if err := stream.Send(&messages[i]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatalf("Failed to listen %v", err)
+	}
+	s := &server{}
+	grpcServer := grpc.NewServer()
+	pb.RegisterServerStreamingServer(grpcServer, s)
+	log.Printf("Starting server. Listening on port %s.", strings.Split(*addr, ":")[1])
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("%v", err)
+	}
+}
